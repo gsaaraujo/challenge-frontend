3a42126e308512c8577de84a7b44875a2940ba54
Gabriel Araujo <gabriel.houth@gmail.com>
2021-09-30 17:40:27 -0300

    feat: set global configs

--- src/constants/app_assets_path.ts
+++
@@ -1,9 +0,0 @@
-export const AppAssetsPath = {
-  arrowDownIcon: '../assets/icons/arrow-down.svg',
-  EyeOnIcon: '../assets/icons/eye-on.svg',
-  EyeOffIcon: '../assets/icons/eye-off.svg',
-  logoIcon: '../assets/icons/logo.svg',
-  userIcon: '../assets/icons/user.svg',
-  defaultUserPhoto: '../assets/images/defaultUserPhoto.jpg',
-  noUser: '../assets/images/no-user.jpg',
-}
