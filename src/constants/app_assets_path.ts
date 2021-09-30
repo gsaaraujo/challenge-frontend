6150393795ebd6dac721533a6cd770ad8cf5051a
Gabriel Araujo <gabriel.houth@gmail.com>
2021-09-30 13:57:12 -0300

    feat: initial commit

---
+++ src/constants/app_assets_path.ts
@@ -0,0 +1,9 @@
+export const AppAssetsPath = {
+  arrowDownIcon: '../assets/icons/arrow-down.svg',
+  EyeOnIcon: '../assets/icons/eye-on.svg',
+  EyeOffIcon: '../assets/icons/eye-off.svg',
+  logoIcon: '../assets/icons/logo.svg',
+  userIcon: '../assets/icons/user.svg',
+  defaultUserPhoto: '../assets/images/defaultUserPhoto.jpg',
+  noUser: '../assets/images/no-user.jpg',
+}
---
+++ src/constants/app_colors.ts
@@ -0,0 +1,14 @@
+export const AppColors = {
+  header: '#464646',
+  footer: '#464646',
+
+  background: '#FFFFFFF',
+  inputBackground: '#F1F1F1',
+
+  heading100: '#464646',
+  heading0: '#FFFFFF',
+
+  placeholer: '#A8A8A8',
+  warning: '#BA3333',
+
+}
